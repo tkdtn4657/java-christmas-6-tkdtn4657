c6af6bf9e9c3403e2452effa414f7465096ff4ed
tkdtn4657 <tkdtn101226@gmail.com>
2023-11-14 00:04:38 +0900

    test(MenuConvertorTest): 입력형태 검증 추가

--- src/test/java/christmas/model/menu/MenuConvertorTest.java
+++ src/test/java/christmas/model/menu/MenuConvertorTest.java
@@ -1,14 +1,19 @@
 package christmas.model.menu;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import christmas.Application;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class MenuConvertorTest {
-
+class MenuConvertorTest extends NsTest {
+    private static final String HYPHEN = "-";
     private final MenuConvertor menuConvertor = new MenuConvertor();
     private final List<String> menus = Arrays.stream(Menu.values()).map(String::valueOf).toList();
     @Test
@@ -17,23 +22,51 @@ class MenuConvertorTest {
 
         assertTrue(menuConvertor.validate(testLines));
     }
+    @Test
+    void 중복_메뉴_테스트() {
+        assertSimpleTest(() -> {
+            runException("1", "티본스테이크-1,티본스테이크-1");
+            assertThat(output()).contains("[ERROR]");
+        });
+    }
 
     @Test
-    void regexTest(){
-        String MENU_REGEX = "^.*-[0-9]+$";
-        String tibon = "티본스테이크--11";
+    void 메뉴에_없는_음식() {
+        assertSimpleTest(() -> {
+            runException("1", "티본스테이크-1,파스타-1");
+            assertThat(output()).contains("[ERROR]");
+        });
+    }
 
-        assertTrue(tibon.matches(MENU_REGEX));
+    @Test
+    void 메뉴_입력형태_검증_파스타1() {
+        assertSimpleTest(() -> {
+            runException("1", "파스타1");
+            assertThat(output()).contains("[ERROR]");
+        });
+    }
+
+    @Test
+    void 메뉴_입력형태_검증_티본스테이크_111(){
+        String MENU_REGEX = ".*-\\d{1,2}$";
+        String tibon = "티본스테이크-111";
+
+        assertFalse(tibon.matches(MENU_REGEX));
     }
 
     @Test
     void menuContain(){
         List<String> testLines = List.of("티본스테이크-1");
         List<String> menuSplitLine = testLines.stream()
-                .map((line) -> line.split("-")[0])
+                .map((line) -> line.split(HYPHEN)[0])
                 .toList();
 
         assertTrue(menus.contains(menuSplitLine.get(0)));
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
     }
 
 }
