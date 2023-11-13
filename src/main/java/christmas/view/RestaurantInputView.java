1abcc7870e0462541961ea907feae60c8c86b5e5
tkdtn4657 <tkdtn101226@gmail.com>
2023-11-13 17:26:54 +0900

    feat(RestaurantInputView): 입력값 처리기능 추가

--- src/main/java/christmas/view/RestaurantInputView.java
+++ src/main/java/christmas/view/RestaurantInputView.java
@@ -3,12 +3,16 @@ package christmas.view;
 import camp.nextstep.edu.missionutils.Console;
 import christmas.convertor.VisitConvertor;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class RestaurantInputView {
 
     private static final String INPUT_VISIT_DAY = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
     private static final String INPUT_ORDER = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
     private final VisitConvertor visitConvertor;
+
     public RestaurantInputView(VisitConvertor visitConvertor){
         this.visitConvertor = visitConvertor;
     }
@@ -26,9 +30,9 @@ public class RestaurantInputView {
         return Integer.parseInt(visitLine);
     }
 
-    public String orderInput() {
+    public List<String> orderInput() {
         System.out.println(INPUT_ORDER);
-        String orderLine = Console.readLine();
-        return orderLine;
+        String inputLine = Console.readLine();
+        return Arrays.stream(inputLine.split(",")).toList();
     }
 }
