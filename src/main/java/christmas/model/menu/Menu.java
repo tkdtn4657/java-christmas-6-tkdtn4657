770fbcd3d5d223745f946a7ae2882ccccdf97fa1
tkdtn4657 <tkdtn101226@gmail.com>
2023-11-13 20:19:26 +0900

    feat(MenuConvertor): 메뉴의 입력형태가 일치하는지 확인기능 추가

---
+++ src/main/java/christmas/model/menu/Menu.java
@@ -0,0 +1,27 @@
+package christmas.model.menu;
+
+import java.util.List;
+
+public enum Menu {
+    양송이수프,
+    타파스,
+    시저샐러드,
+    티본스테이크,
+    바비큐립,
+    해산물파스타,
+    크리스마스파스타,
+    초코케이크,
+    아이스크림,
+    제로콜라,
+    레드와인,
+    샴페인;
+
+    private Menu(){
+
+    }
+
+
+
+
+}
+
---
+++ src/main/java/christmas/model/menu/MenuConvertor.java
@@ -0,0 +1,39 @@
+package christmas.model.menu;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class MenuConvertor {
+
+    private static final String MENU_REGEX = "^.*-[0-9]";
+
+    private final List<String> menus = Arrays.stream(Menu.values())
+            .map(String::valueOf)
+            .toList();
+
+    private final List<Consumer<List<String>>> visitFilters = List.of(
+            this::menuFormValidate
+    );
+
+    public boolean validate(List<String> splitLine){
+        try {
+            visitFilters.forEach(
+                    filter -> filter.accept(splitLine));
+        }catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    private void menuFormValidate(List<String> splitLine){
+        if(splitLine.stream()
+                .filter((line)-> line.matches(MENU_REGEX))
+                .toList()
+                .size() != splitLine.size()){
+            throw new IllegalArgumentException();
+        }
+    }
+
+}
