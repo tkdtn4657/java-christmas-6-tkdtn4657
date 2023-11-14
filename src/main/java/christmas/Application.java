ab18331d3ff960001e71dec73cb83d83f3921aa8
tkdtn4657 <tkdtn101226@gmail.com>
2023-11-14 12:40:34 +0900

    refactor(Application): 변경된 생성자 추가

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -2,16 +2,20 @@ package christmas;
 
 import christmas.controller.Employee;
 import christmas.convertor.VisitConvertor;
+import christmas.model.Restaurant;
+import christmas.model.menu.MenuConvertor;
 import christmas.view.RestaurantInputView;
 import christmas.view.RestaurantOutputView;
 
 public class Application {
     public static void main(String[] args) {
-        Employee restaurant = new Employee(
+        Employee employee = new Employee(
                 new RestaurantInputView(new VisitConvertor()),
-                new RestaurantOutputView()
+                new RestaurantOutputView(),
+                new MenuConvertor(),
+                new Restaurant()
         );
 
-        restaurant.run();
+        employee.run();
     }
 }
