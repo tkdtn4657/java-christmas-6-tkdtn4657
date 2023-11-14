013edd1bfbce484392b425a61e73cf16e5e2dd9a
tkdtn4657 <tkdtn101226@gmail.com>
2023-11-14 13:21:29 +0900

    fix(Menu): 티본스테이크의 menuType 오타수정

--- src/main/java/christmas/model/menu/Menu.java
+++ src/main/java/christmas/model/menu/Menu.java
@@ -1,6 +1,5 @@
 package christmas.model.menu;
 
-import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.List;
 
@@ -8,7 +7,7 @@ public enum Menu {
     양송이수프(6000, "애피타이저"),
     타파스(5500, "애피타이저"),
     시저샐러드(8000, "애피타이저"),
-    티본스테이크(55000, "멩ㄴ"),
+    티본스테이크(55000, "메인"),
     바비큐립(54000, "메인"),
     해산물파스타(35000, "메인"),
     크리스마스파스타(25000, "메인"),
@@ -22,19 +21,28 @@ public enum Menu {
     private final int price;
     private final String menuType;
 
-    Menu(int price, String menuType){
+    Menu(int price, String menuType) {
         this.price = price;
         this.menuType = menuType;
     }
 
-    public static boolean isOnlyDrinks(List<Menu> menus, EnumMap<Menu, Integer> orderSheet){
+    public static boolean isOnlyDrinks(List<Menu> menus, EnumMap<Menu, Integer> orderSheet) {
         return menus.stream()
                 .filter(menu ->
                         orderSheet.get(menu) >= ONE
-                        && !menu.menuType.equals("음료")
+                                && !menu.menuType.equals("음료")
                 )
                 .toList()
                 .size() < ONE;
+    }
+
+    public static List<Menu> availableMenus(List<Menu> menus, EnumMap<Menu, Integer> orderSheet) {
+        return menus.stream()
+                .filter(menu ->
+                        orderSheet.get(menu) >= ONE
+                                && !menu.menuType.equals("음료")
+                )
+                .toList();
     }
 }
 
